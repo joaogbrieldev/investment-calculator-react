0d6770534a906c15947442fc25d32b2893872868
jooaogbriel <joao.ggwp@outlook.com>
2024-08-29 20:03:32 -0300

    feat: create new component
      i create the results component to show the returns investment user

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Header } from "./components/Header";
+import { Results } from "./components/Result";
 import { UserInput } from "./components/UserInput";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <>
       <Header />
       <UserInput />
+      <Results />
     </>
   );
 }
